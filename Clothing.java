626d343d0714056b89e67a2b5261ec5a3a9ab747
anniepauline <anniepauline.23@gmail.com>
2023-10-28 20:39:08 +0530

    initial commit

---
+++ Clothing.java
@@ -0,0 +1,9 @@
+package flipkartWOpps;
+public class Clothing extends Product {
+    String size;
+
+    Clothing(String n, double p, int q, String s) {
+        super(n, p, q);
+        this.size = s;
+    }
+}
