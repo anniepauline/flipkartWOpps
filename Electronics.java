7bdc92b8ae56729200ca432e648ec717bdeddae9
anniepauline <anniepauline.23@gmail.com>
2023-10-28 20:25:03 +0530

    initial commit

---
+++ Electronics.java
@@ -0,0 +1,9 @@
+package flipkartWOpps;
+public class Electronics extends Product {
+    double durability;
+
+    Electronics(String n, double p, int q, double d) {
+        super(n, p, q);
+        this.durability = d;
+    }
+}
